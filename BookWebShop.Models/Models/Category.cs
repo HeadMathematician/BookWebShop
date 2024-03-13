00109dc0b9dfb7910dbf1b425c253a0a53f2db1b
Teo Kupčinovac <teo@insa-software.com>
2024-03-13 18:18:50 +0100

    Add project files.

---
+++ BookWebShop.Models/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookWebShop.Models.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
---
+++ BookWebShop/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using BookWebShop.Data;
+using BookWebShop.Models.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWebShop.Controllers
+{
+
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int categoryId)
+        {
+            var toDelete = _context.Categories.Where(x => x.Id == categoryId).FirstOrDefault();
+
+            if (toDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(toDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Edit(int categoryId)
+        {
+            if(categoryId == 0)
+            {
+                return NotFound();
+            }
+            Category category = _context.Categories.Find(categoryId);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            
+            Category toUpdate = _context.Categories.Where(x => x.Id == category.Id).FirstOrDefault();
+
+            toUpdate.Name = category.Name;
+            toUpdate.DisplayOrder = category.DisplayOrder;
+
+            _context.SaveChanges();
+            TempData["success"] = "Category edited succesfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
